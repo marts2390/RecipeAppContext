719f8091f2a65f3e029a92d15015e768bdb93879
Chris Martyr <chris@MacBook-Pro.local>
2021-02-25 08:44:29 +0000

    remove unused code

--- src/actions/layerInterface.ts
+++ src/actions/layerInterface.ts
@@ -1,20 +1,5 @@
 import { Dispatch } from 'react'
 
-// import { LocaleModel } from '@models/localeModel'
-// import { ThemeModel } from '@models/themeModel'
-// import { ILoginFormDataModel } from '@models/loginFormDataModel'
-// import { IMessageItemModel } from '@models/messagesModel'
-// import { IRegisterFormDataModel } from '@models/registerFormDataModel'
-// import { IRecoveryFormDataModel } from '@models/recoveryFormDataModel'
-// import { AuthFormTypesModel } from '@models/authFormTypesModel'
-// import { IRepresentation, IGeneration } from '@src/models/ExplorerAssetReportModels'
-// import { IExplorerTreeDataModel } from '@src/models/ExplorerTreeModel'
-// import { IDublunCoreMetadataDTM } from '@src/models/dtm/DublinCoreMetadataDtm'
-// import { IModsMetadataDTM } from '@models/dtm/ModsMetadataDtm'
-// import { IInformationObjectDTM } from '@models/dtm/InformationObjectDTM'
-// import { entityTypes } from '@models/preservicaApiContracts/contentModels'
-
-
 export interface IActions {
   HomeScreenService: IHomeScreenService;
   RecipeScreenService: IRecipeScreenService;
